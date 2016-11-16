3cdd216a0fc7fec8faf7f9af08bfc5b660c38445
Kevin1995 <kevinkehoe18@gmail.com>
2016-11-16 11:38:07 +0000

    Update 2
    Enemy script added to follow each wave point until it reaches the end
    where the enemy will destroy it self
    
    Next is to do a wave spawner for the enemies.

---
+++ Tower-Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = WavePoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if(wavepointIndex >= WavePoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = WavePoints.points[wavepointIndex];
+    }
+
+}
